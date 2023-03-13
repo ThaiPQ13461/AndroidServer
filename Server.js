7d9f2ac73c7104b1eb7f0cfb8f80a42b83850ca5
ASUS <thaipqph13461@fpt.edu.vn>
2023-03-13 07:38:53 +0700

    BTVN3

--- Server.js
+++ Server.js
@@ -3,22 +3,22 @@ var dt = require("./uniTest");
 var fs = require("fs");
 http
   .createServer(function (req, res) {
-    fs.appendFile("mynewfile1.txt", "Hello content!", function (err) {
-      if (err) throw err;
-      console.log("Saved!");
-    });
-    fs.readFile("index.html", function (err, data) {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      return res.end();
-    });
+    // fs.appendFile("mynewfile1.txt", "Hello content!", function (err) {
+    //   if (err) throw err;
+    //   console.log("Saved!");
+    // });
+    // fs.readFile("index.html", function (err, data) {
+    //   res.writeHead(200, { "Content-Type": "text/html" });
+    //   res.write(data);
+    //   return res.end();
+    // });
     // fs.unlink("mynewfile1.txt", function (err) {
     //   if (err) throw err;
     //   console.log("File deleted!");
     // });
-    //res.writeHead(200, { "Content-Type": "text/plain" });
-    // res.write("Thoi gian hien tai: " + dt.myDataTime() + "");
-    // res.write("\n Tong: " + dt.sum(5, 5));
-    // res.end();
+    res.writeHead(200, { "Content-Type": "text/plain" });
+     res.write("Hello world ");
+     res.write("\n Tong: " + dt.sum(5, 5));
+     res.end();
   })
   .listen(3000);
